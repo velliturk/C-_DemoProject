daab21894218688818e00bd02477781b655bb5d2
Veli Türk <veturk34@gmail.com>
2022-08-11 15:33:57 +0300

    initial commit
    initial commit

---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    internal class UserValidationManager:IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1958 && gamer.FirstName == "VELİ" && gamer.LastName == "TÜRK" &&
+                gamer.IdentityNumber == 2132432412)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
